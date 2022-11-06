1d30e788d3500dc8bc1b7297ca346ea10f2c3083
msfm2018 <msfm@163.com>
2022-11-06 19:45:32 +0800

    up

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import * as THREE from "three";
 import gsap from "gsap";
 import * as dat from "dat.gui";
-import {loadGlb, WxpKeyPressed1, processing, frameArea} from "./public/resources/common/utils.js";
-import {Control, Light, Base, floor} from "./public/resources/common/base.js";
+import { loadGlb, WxpKeyPressed1, processing, frameArea } from "./public/resources/common/utils.js";
+import { Control, Light, Base, floor } from "./public/resources/common/base.js";
 import global from "./public/resources/common/global.js";
 import Vglobal from "./public/resources/common/vglobal.js";
 
 
 let vg = new Vglobal();
+
+//点击提示
+let show1;
+
 const init = () => {
+
+
+
 
     [vg.renderer, vg.scene] = Base(render)
     document.body.appendChild(vg.renderer.domElement);
@@ -21,8 +28,8 @@ const init = () => {
 
     let glb = loadGlb('model', "CheJian.glb", false, 1, 0);
     glb.then(gltf => {
-        // setModelPosition(gltf.scene);
-        console.log(gltf);
+
+
         vg.mixer = new THREE.AnimationMixer(gltf.scene);
         var AnimationAction = vg.mixer.clipAction(gltf.animations[0]);
         AnimationAction.timeScale = 0.7; //默认1，可以调节播放速度
@@ -60,9 +67,21 @@ const init = () => {
 
         vg.scene.add(mroot);
 
+        // //准备显示
+        show1 = document.querySelector('#show1');
+
+
         document.body.addEventListener('click', selectHandler, false);
 
         function selectHandler(ev) {
+            var x, y;
+            if (ev.changedTouches) {
+                x = ev.changedTouches[0].pageX;
+                y = ev.changedTouches[0].pageY;
+            } else {
+                x = ev.clientX;
+                y = ev.clientY;
+            }
             vg.mouse.x = (ev.clientX / window.innerWidth) * 2 - 1;
             vg.mouse.y = -(ev.clientY / window.innerHeight) * 2 + 1;
 
@@ -72,6 +91,9 @@ const init = () => {
 
             if (intersects.length > 0) {
                 console.log(intersects[0].object.name);
+
+
+
                 if ((intersects[0].object.name != 'Plane008') &&
                     (intersects[0].object.name != 'Plane008_1') &&
                     (intersects[0].object.name != 'camera-hj_dg') &&
@@ -88,9 +110,30 @@ const init = () => {
                     global.selectedObjects.material = vg.fixMaterial;
 
                     global.preSelectedObjects = global.selectedObjects;
-                } else {
+    //  //准备显示
+    //  show1 = document.querySelector('#show1');
+                    //找到删除 避免重复
+                    var el = document.getElementById('login');
+                    if (el)
+                        el.parentNode.removeChild(el);
 
+                    show1.style.display = "block";
+                    show1.style.left = x + "px";
+                    show1.style.top = y - 100 + "px";
+                    var link = document.createElement('a');
+                    link.setAttribute('href', '#');
+                    link.setAttribute('id', 'login');
+                    link.style.color = 'green';
+                    link.innerHTML = '登录';
+                    show1.appendChild(link);
+
+                    // document.querySelector("#info").innerText="选中："+intersects[0].object.name;
+
+                } else {
+                    show1.style.display = "none"
                 }
+            } else {
+                show1.style.display = "none"
             }
         }
 
@@ -106,7 +149,7 @@ const init = () => {
         let btn2 = document.querySelector('#btn2');
         btn2.addEventListener('click', evt => {
             if (global.obj[global.selectedObjects.name]) {
-                gsap.to(global.selectedObjects.position, {y: global.obj[global.selectedObjects.name].y, duration: 2,});
+                gsap.to(global.selectedObjects.position, { y: global.obj[global.selectedObjects.name].y, duration: 2, });
                 delete global.obj[global.selectedObjects.name];
                 global.selectedObjects.material = new THREE.MeshPhysicalMaterial({
                     color: 0xffff00, metalness: 0.6, roughness: 0.4, clearcoat: 0.05, clearcoatRoughness: 0.05
@@ -120,7 +163,7 @@ const init = () => {
             if (!global.obj[global.selectedObjects.name]) {
                 let vv = global.selectedObjects.clone();
                 global.obj[global.selectedObjects.name] = vv.position;
-                gsap.to(global.selectedObjects.position, {y: 16, duration: 2,});
+                gsap.to(global.selectedObjects.position, { y: 16, duration: 2, });
                 // gsap.to(global.selectedObjects.position, { y: 16,  duration: 2, yoyo: true, repeat: -1 });
                 global.selectedObjects.material = new THREE.MeshPhysicalMaterial({
                     color: 0xffff00, metalness: 0.6, roughness: 0.4, clearcoat: 0.05, clearcoatRoughness: 0.05
